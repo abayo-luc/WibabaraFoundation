ee7e3b0c450de06bee4d09902ea61faa3ebb421d
JoeMugwaneza <muganezajoseph@gmail.com>
2017-11-17 23:15:36 +0200

    completed crud functionalities of events and contacts with user from devise gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+
+  devise_for :users
+  root to: 'pages#index'
+
+  resources :events
+  resources :contacts, :only => [:index, :new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
